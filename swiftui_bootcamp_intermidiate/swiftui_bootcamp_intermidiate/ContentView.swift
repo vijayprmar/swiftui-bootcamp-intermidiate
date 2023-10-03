55cfe177350835e1c8cf3493ee33c4a3f52f4917
Vijay Parmar <vijay@ennomail.com>
2023-10-03 18:18:26 +0530

    Initial Commit

---
+++ swiftui_bootcamp_intermidiate/swiftui_bootcamp_intermidiate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui_bootcamp_intermidiate
+//
+//  Created by Vijay Parmar on 03/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui_bootcamp_intermidiate/swiftui_bootcamp_intermidiate/swiftui_bootcamp_intermidiateApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_bootcamp_intermidiateApp.swift
+//  swiftui_bootcamp_intermidiate
+//
+//  Created by Vijay Parmar on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_bootcamp_intermidiateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
